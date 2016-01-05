6c6e7ebe0b6aa6a04d6e6823bc2e8230161bf470
alex davis <alex@alex2006hw.com>
2016-01-04 22:52:33 -0800

    initial-boilderplate

---
+++ entry/server/webpack.conf.js
@@ -0,0 +1,16 @@
+var webpack = require('webpack');
+
+var babelSettings = { presets: ['react', 'es2015', 'stage-0'] };
+babelSettings.plugins = ['transform-decorators-legacy'];
+
+module.exports = {
+  entry: './entry',
+  module: {
+    loaders: [
+      { test: /\.jsx?$/, loader: 'babel', query: babelSettings, exclude: /node_modules/ },
+      { test: /\.css$/, loader: 'null' },
+      { test: /\.(png|jpe?g)(\?.*)?$/, loader: 'url?limit=8182' },
+      { test: /\.(svg|ttf|woff|eot)(\?.*)?$/, loader: 'file' }
+    ]
+  }
+};
